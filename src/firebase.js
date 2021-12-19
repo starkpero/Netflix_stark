ba30f53e8fa1c03df772fa314f944ed1c42916be
starkpero <anishgupta0066@gmail.com>
2021-12-19 19:58:35 +0530

    Initializing app! Lesssgoooo

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBWBDCNnQozLaDAFkS5LH2Cb_eASRFB4Qw",
+    authDomain: "netflix-5fb68.firebaseapp.com",
+    projectId: "netflix-5fb68",
+    storageBucket: "netflix-5fb68.appspot.com",
+    messagingSenderId: "1002310242448",
+    appId: "1:1002310242448:web:0c455fa42298eb10088815"
+  };
