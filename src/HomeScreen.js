3ce2b5a9225f2fa72cefb3032cf75af5bc5dfe2c
starkpero <anishgupta0066@gmail.com>
2021-12-20 20:37:35 +0530

    completed HomeScreen component!

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -12,6 +12,7 @@ function HomeScreen() {
 
             <Banner/>
 
+            <Row title='NETFLIX ORIGINALS' fetchUrl = {requests.fetchNetflixOriginals} isLargeRow/>
             <Row title='Trending Now' fetchUrl = {requests.fetchTrending}/>
             <Row title='Top Rated' fetchUrl = {requests.fetchTopRated} />
             <Row title='Action Movies' fetchUrl = {requests.fetchActionMovies} />
