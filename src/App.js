63bf946bde9c41a68aeee6b9d972bb3cf2ff006f
starkpero <anishgupta0066@gmail.com>
2021-12-28 00:22:01 +0530

    implemented redux

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
-import {auth} from './firebase'
+import {auth} from './firebase';
+import {login, logout, selectUser } from './features/userSlice';
+import {useDispatch, useSelector } from 'react-redux';
+import ProfileScreen from './ProfileScreen';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
       const unsubscribe = auth.onAuthStateChanged(userAuth =>{
         if(userAuth){
           //Logged In
-          console.log(userAuth);
+          //console.log(userAuth);
+          dispatch(login({
+            uid : userAuth.uid,
+            email : userAuth.email,
+          })
+          );
         }
         else{
           //Logged out
+          dispatch(logout);
         }
       });
     return unsubscribe;
@@ -28,6 +38,9 @@ function App() {
           <LoginScreen/> 
          ) : (
          <Switch>
+           <Route path='/profile'>
+             <ProfileScreen/>
+           </Route>
           <Route exact path="/">
             <HomeScreen/>
           </Route>
